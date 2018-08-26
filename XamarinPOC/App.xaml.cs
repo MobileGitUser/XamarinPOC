8c351c48cd3de4c03edf4d8acf295772032e91e6
MEA\VijayaKumar.Ravuru <vijayakumar600@gmail.com>
2018-08-26 15:37:26 +0530

    Intial Commit

---
+++ XamarinPOC/App.xaml.cs
@@ -0,0 +1,16 @@
+using MvvmCross.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XamarinPOC
+{
+    public partial class App 
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ XamarinPOC/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+
+namespace XamarinPOC.ViewModels
+{
+    public class WelcomeViewModel :MvxViewModel
+    {
+        public WelcomeViewModel()
+        {
+        }
+
+
+        public override Task Initialize()
+        {
+            //TODO: Add logic here
+            return base.Initialize();
+        }
+
+
+        private string _eyText = "Hello EY";
+        public string EYText
+        {
+            get { return _eyText; }
+            set { SetProperty(ref _eyText, value); }
+        }
+    }
+}
